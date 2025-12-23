72255fc57b8d7af01c16fa126a7878a0ad18ce68
Ibrahim Abdi <ibroabdi@outlook.com>
2025-12-23 01:15:21 +0000

    begun initialising canvas

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+
+const Initialise = async () => {
+    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+    const adapter = await navigator.gpu?.requestAdapter();
+    const device = await adapter?.requestDevice();
+
+    if (!device){
+        throw new Error('WebGPU is not supported by this browser.');
+    }
+
+    console.log('WebGPU adapter and device initialised:', adapter?.info, device);
+
+    const context = canvas.getContext('webgpu')!;
+    console.log('canvas context obtained:', context);
+    const format = navigator.gpu.getPreferredCanvasFormat();
+
+    context.configure({
+        device,
+        format,
+        alphaMode: 'premultiplied',
+    });
+
+    console.log('canvas context configured.')
+}
+
+Initialise();
