3a288252f5b21ad9d2cf640c5e88e727a85f7d7d
Ibrahim Abdi <ibroabdi@outlook.com>
2025-12-25 16:53:45 +0000

    canvas and background colour changed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-
 const Initialise = async () => {
     const canvas = document.getElementById('canvas') as HTMLCanvasElement;
     const adapter = await navigator.gpu?.requestAdapter();
     const device = await adapter?.requestDevice();
+
+    console.log('WebGPU Device: ', device);
 
     if (!device){
         throw new Error('WebGPU is not supported by this browser.');
@@ -20,7 +21,22 @@ const Initialise = async () => {
         alphaMode: 'premultiplied',
     });
 
-    console.log('canvas context configured.')
+    console.log('canvas context configured.');
+
+    const commandEncoder = device.createCommandEncoder();
+    const textureView = context.getCurrentTexture().createView();
+
+    const renderPass = commandEncoder.beginRenderPass({
+        colorAttachments: [{
+            view: textureView,
+            clearValue: {r: 0.4, g: 0.4, b: 0.4, a: 1.0},
+            loadOp: 'clear',
+            storeOp: 'store',
+        }],
+    });
+    renderPass.end();
+
+    device.queue.submit([commandEncoder.finish()]);
 }
 
 Initialise();
